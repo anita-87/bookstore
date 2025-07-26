def82680e906cf42d66c312de6a88d83a52ef040
anita-87 <ana.rodriguez.fernandez.87@gmail.com>
2025-07-26 19:02:33 +0200

    First drop of the book service

---
+++ books/src/main/java/com/bookstore/books/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.bookstore.books.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Controller
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
